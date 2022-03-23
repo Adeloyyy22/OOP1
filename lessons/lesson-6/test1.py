73b523cc9fa66a1af2fe380b2d1a8c17fe7feb2b
Adelya <meimanbekova2210@gmail.com>
2022-03-23 17:32:22 +0600

    lesson6

--- lessons/lesson-6/test1.py
+++ lessons/lesson-6/test1.py
@@ -1,7 +1,20 @@
-mass = [i for i in range(1_000_000)]
+def get_list() -> list:
+    return list(range(0, 1_000_000_000, 2))
 
-target = 1_000_000
 
-for num in range(len(mass)):
-    if target == mass[num]:
-        print(num)
+"""
+    what is Binary Search?
+    then do Solution for search target in list
+"""
+
+
+class Solution:
+    """
+        find_target -> YOUR_CODE
+    """
+
+    def find_target(self, list, target):
+        pass
+
+
+Solution().find_target(get_list(),888_888)
